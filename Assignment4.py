ee627a9c0ee5a4ad3fe9160b0e75a0f1a1ca43e1
dvb <diegoverab@gmail.com>
2017-10-15 16:57:36 -0300

    ha2

---
+++ Assignment4.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+
+df = pd.read_csv('data michigan.csv',sep=';')
+df['month_year'] = df['Day'].apply(lambda date: date[3:])
+
+AnnArbor = df[df['City'] == 'Ann Arbor'].groupby('month_year').aggregate({'High':np.mean})
+Lansing = df[df['City'] == 'Lansing'].groupby('month_year').aggregate({'High':np.mean})
+
+plt.figure()
+plt.plot_date(x=AnnArbor.index, y=AnnArbor.High, fmt="r-", label= 'Ann Arbor')
+plt.plot_date(x=Lansing.index, y=Lansing.High, fmt="b-", label= 'Lasing')
+plt.title("Monthly average of maximum temperatures (2017)")
+plt.ylabel("Average temperatures (°F)")
+plt.xlabel("Month")
+plt.ylim([0,90])
+plt.xticks(rotation=45)
+plt.grid(True)
+plt.legend()
+plt.show()
+
